ec80655c4f0e96e2289e14cbc328143d60ef760a
pinyuan1 <50605411+pinyuan1@users.noreply.github.com>
2020-03-07 12:32:22 +0800

    groceryproject

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button v-on:click="counter = counter + 1"> +</button>
+        <a>{{ counter }}</a>
+        <button v-on:click="counter = counter - 1"> -</button>
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+        counter:0
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped> 
+ a{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center; 
+  color: black; }
+</style>
